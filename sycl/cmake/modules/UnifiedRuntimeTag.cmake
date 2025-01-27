fd9240ff92da98981a22c926a76a2d22eb1644b7
Rafal Rudnicki <rafal.rudnicki@intel.com>
2025-01-27 16:05:41 +0000

    update ur tag

--- sycl/cmake/modules/UnifiedRuntimeTag.cmake
+++ sycl/cmake/modules/UnifiedRuntimeTag.cmake
@@ -1,7 +1,2 @@
-# commit 0bb6789f0113ea937d861fd67fd677b91ecdeb8b
-# Merge: e370a2b9 eeff9f4a
-# Author: Kenneth Benzie (Benie) <k.benzie@codeplay.com>
-# Date:   Mon Jan 27 10:40:02 2025 +0000
-#     Merge pull request #2551 from przemektmalon/przemek/bindless-images-host-usm
-#     Enable creation of bindless images backed by host USM
-set(UNIFIED_RUNTIME_TAG f6bd411c9b44cdcc7acca49e0c0e0efb43825d34)
+
+set(UNIFIED_RUNTIME_TAG 07001aa737ef66f394cd36f2b56f28b97e98c1c2)
